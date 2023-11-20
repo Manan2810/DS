97c0d80f9b679d2cecc27d82f86565ee4523e3bf
Manan Mehra <mananmehra330@gmail.com>
2023-11-21 01:51:05 +0530

    Assignment 1 added

---
+++ Assignment1.R
@@ -0,0 +1,62 @@
+#Q1
+A=23.4
+B=45
+C=678
+print(A)
+print(B)
+print(C)
+
+#Q2 ls() is used to display entire varibles on the Screen
+
+#Q3 rm(varibale name) to delete a single variable
+  # rm(list=ls()) used to delete all the variables
+
+#Q4 Create a comment “I am learning R”
+#Q5
+firstname="Manan"
+lastname="Mehra"
+
+#Q6
+my_variable <- 0
+print(my_variable)
+# Assigning 1 to a variable
+my_variable <- 1
+print(my_variable)
+# Using logical values
+logical_variable <- TRUE  # Equivalent to 1
+print(logical_variable)
+logical_variable <- FALSE  # Equivalent to 0
+print(logical_variable)
+
+#Q8
+# Applying functions
+result_exp <- exp(2)   # Exponential function (e^2)
+result_log <- log(10)   # Natural logarithm
+result_log10 <- log10(100)  # Base 10 logarithm
+result_log2 <- log2(8)   # Base 2 logarithm
+result_sqrt <- sqrt(25)   # Square root
+result_pi <- pi   # Value of pi
+
+# Printing results
+print("Results of Functions:")
+print(result_exp)
+print(result_log)
+print(result_log10)
+print(result_log2)
+print(result_sqrt)
+print(result_pi)
+
+#Q9
+expression1 <- 23 + (4.5 * 2.3) / 10
+expression2 <- 456 / 12 - log(90)
+expression3 <- exp(5) + 12 / (5^6)
+expression4 <- sqrt(45) * 12 / 3
+
+# Printing results of expressions
+print("Results of Expressions:")
+print(expression1)
+print(expression2)
+print(expression3)
+print(expression4)
+
+
